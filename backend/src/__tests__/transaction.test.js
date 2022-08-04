fe3338c0242c633d040e890caa81091e51b7a83d
Chenjia Li <cligrenet@gmail.com>
2022-08-04 10:13:05 +0200

    Create api e2e testing for user and transaction

--- backend/src/__tests__/transaction.test.js
+++ backend/src/__tests__/transaction.test.js
@@ -20,12 +20,20 @@ const transaction1 = {
 };
 
 let token = null;
+let transactionId = null;
+let userId = null;
 
 beforeAll(async () => {
 	const salt = await bcrypt.genSalt(10);
 	const hashedPassword = await bcrypt.hash(user1.password, salt);
 
-	await prisma.users.create({ data: { name: user1.name, email: user1.email, password: hashedPassword } });
+	await prisma.users.create({
+		data: {
+			name: user1.name,
+			email: user1.email,
+			password: hashedPassword,
+		},
+	});
 
 	const res = await supertest(app)
 		.post('/api/v1/users/login')
@@ -34,7 +42,9 @@ beforeAll(async () => {
 		.set('Accept', 'application/json')
 		.expect(200);
 
-	token = res.body.token;
+	token = await res.body.token;
+
+	userId = await res.body.user_id;
 });
 
 afterAll(async () => {
@@ -42,14 +52,70 @@ afterAll(async () => {
 });
 
 describe('Transaction', () => {
+	describe('get empty transactions', () => {
+		it('should get empty transaction', async () => {
+			const res = await supertest(app)
+				.get('/api/v1/transactions')
+				.set('Content-Type', 'application/json')
+				.set('Accept', 'application/json')
+				.set('Authorization', 'Bearer ' + token)
+				.expect(200);
+
+			const transactions = await res.body.data;
+			expect(transactions).toEqual([]);
+		});
+	});
+
+	describe('add a transaction', () => {
+		it('should create transaction', async () => {
+			const res = await supertest(app)
+				.post('/api/v1/transactions')
+				.set('Content-Type', 'application/json')
+				.set('Accept', 'application/json')
+				.set('Authorization', 'Bearer ' + token)
+				.send({
+					user_id: userId,
+					text: transaction1.text,
+					amount: transaction1.amount,
+					category: transaction1.category,
+				})
+				.expect(201);
+
+			transactionId = await res.body.data.transaction_id;
+		});
+	});
+
 	describe('get all transactions', () => {
-		test('It should fetch all transactions', async () => {
+		it('should fetch all transactions', async () => {
 			await supertest(app)
 				.get('/api/v1/transactions')
 				.set('Content-Type', 'application/json')
 				.set('Accept', 'application/json')
 				.set('Authorization', 'Bearer ' + token)
 				.expect(200);
+		});
+	});
+
+	describe('delete a transaction', () => {
+		it('should delete the transaction by id', async () => {
+			await supertest(app)
+				.delete('/api/v1/transactions/' + transactionId)
+				.set('Content-Type', 'application/json')
+				.set('Accept', 'application/json')
+				.set('Authorization', 'Bearer ' + token)
+				.expect(200);
+		});
+
+		it('should get empty transactions', async () => {
+			const res = await supertest(app)
+				.get('/api/v1/transactions')
+				.set('Content-Type', 'application/json')
+				.set('Accept', 'application/json')
+				.set('Authorization', 'Bearer ' + token)
+				.expect(200);
+
+			const transactions = await res.body.data;
+			expect(transactions).toEqual([]);
 		});
 	});
 });
--- backend/src/__tests__/user.test.js
+++ backend/src/__tests__/user.test.js
@@ -6,31 +6,104 @@ const app = createServer();
 
 const prisma = new PrismaClient();
 
-const user1 = {
+const user2 = {
 	name: 'user2',
 	email: 'user2@email.com',
 	password: '123',
 };
 
 beforeAll(async () => {
-	// await prisma.users.create({ data: user1 });
+	// await prisma.users.create({ data: user2 });
 });
 
 afterAll(async () => {
 	await prisma.users.deleteMany({});
 });
 
+let token = null;
+
 describe('User', () => {
 	describe('register a user', () => {
+		it('should throw exception if email empty', async () => {
+			await supertest(app)
+				.post('/api/v1/users')
+				.send({
+					name: user2.name,
+					password: user2.password,
+				})
+				.expect(400);
+		});
+
+		it('should throw exception if password empty', async () => {
+			await supertest(app)
+				.post('/api/v1/users')
+				.send({
+					name: user2.name,
+					email: user2.email,
+				})
+				.expect(400);
+		});
+
+		it('should throw exception if no body provided', async () => {
+			await supertest(app).post('/api/v1/users').expect(400);
+		});
+
 		it('should register a user', async () => {
 			await supertest(app)
 				.post('/api/v1/users')
 				.send({
-					name: user1.name,
-					email: user1.email,
-					password: user1.password,
+					name: user2.name,
+					email: user2.email,
+					password: user2.password,
 				})
 				.expect(201);
+		});
+	});
+
+	describe('login a user', () => {
+		it('should throw exception if email empty', async () => {
+			await supertest(app)
+				.post('/api/v1/users/login')
+				.send({
+					password: user2.password,
+				})
+				.expect(401);
+		});
+
+		it('should throw exception if password empty', async () => {
+			await supertest(app)
+				.post('/api/v1/users/login')
+				.send({
+					email: user2.email,
+				})
+				.expect(401);
+		});
+
+		it('should throw exception if no body provided', async () => {
+			await supertest(app).post('/api/v1/users/login').expect(401);
+		});
+
+		it('should log in a user', async () => {
+			const res = await supertest(app)
+				.post('/api/v1/users/login')
+				.send({
+					email: user2.email,
+					password: user2.password,
+				})
+				.expect(200);
+
+			token = res.body.token;
+		});
+	});
+
+	describe('get current user information', () => {
+		it('should fetch current user data', async () => {
+			await supertest(app)
+				.get('/api/v1/users/me')
+				.set('Content-Type', 'application/json')
+				.set('Accept', 'application/json')
+				.set('Authorization', 'Bearer ' + token)
+				.expect(200);
 		});
 	});
 });
